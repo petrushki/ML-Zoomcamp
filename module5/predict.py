7e5186de4162381cd3f89b9b4faca6d693a9548e
fuj05laf <diana.petrusheva@hhu.de>
2025-10-25 11:06:35 +0200

    virtual env added

--- module5/predict.py
+++ module5/predict.py
@@ -1,36 +1,51 @@
 import pickle
-
+from fastapi import FastAPI
+import uvicorn
+from typing import Dict, Any
 
 model_file = 'model.bin'
+
+app = FastAPI(title = "customer-churn-prediction")
 
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
 
-datapoint = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+def predict_single(customer):
+    
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    return y_pred
 
 
-X = dv.transform(datapoint)
-y_pred = model.predict_proba(X)[0, 1]
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    prediction = predict_single(customer)
+    return {
+        "churn_probability": float(prediction), 
+        "churn": bool(prediction >= 0.5)
+        }
 
-print('Churn probability:', y_pred)
+
+# datapoint = {
+#     'gender': 'female',
+#     'seniorcitizen': 0,
+#     'partner': 'yes',
+#     'dependents': 'no',
+#     'phoneservice': 'no',
+#     'multiplelines': 'no_phone_service',
+#     'internetservice': 'dsl',
+#     'onlinesecurity': 'no',
+#     'onlinebackup': 'yes',
+#     'deviceprotection': 'no',
+#     'techsupport': 'no',
+#     'streamingtv': 'no',
+#     'streamingmovies': 'no',
+#     'contract': 'month-to-month',
+#     'paperlessbilling': 'yes',
+#     'paymentmethod': 'electronic_check',
+#     'tenure': 1,
+#     'monthlycharges': 29.85,
+#     'totalcharges': 29.85
+# }
+
